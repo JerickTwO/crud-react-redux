5b0a16972d3ae642ddb710508e5a794b39a06838
JerickTwO <jerickjjtm774@gmail.com>
2024-08-05 12:59:42 -0500

    add sonner and middelwares from sonner

--- src/store/users/slice.ts
+++ src/store/users/slice.ts
@@ -1,4 +1,25 @@
-import { type PayloadAction, createSlice } from "@reduxjs/toolkit";
+import { createSlice, type PayloadAction } from "@reduxjs/toolkit";
+
+const DEFAULT_STATE = [
+	{
+		id: "1",
+		name: "Yazman Rodriguez",
+		email: "yazmanito@gmail.com",
+		github: "yazmanito",
+	},
+	{
+		id: "2",
+		name: "John Doe",
+		email: "leo@gmail.com",
+		github: "leo",
+	},
+	{
+		id: "3",
+		name: "Haakon Dahlberg",
+		email: "haakon@gmail.com",
+		github: "midudev",
+	},
+];
 
 export type UserId = string;
 
@@ -7,42 +28,37 @@ export interface User {
 	email: string;
 	github: string;
 }
+
 export interface UserWithId extends User {
-	id: string;
+	id: UserId;
 }
 
-const initialState: UserWithId[] = [
-	{
-		id: "1",
-		name: "Miguel Angel",
-		email: "david@gmail.com",
-		github: "midudev",
-	},
-	{
-		id: "2",
-		name: "Maykel Jordan",
-		email: "michael@gmail.com",
-		github: "maykeldev",
-	},
-	{
-		id: "3",
-		name: "Jerick Toro",
-		email: "jerick@gmail.com",
-		github: "JerickTwO",
-	},
-];
+const initialState: UserWithId[] = (() => {
+	const persistedState = localStorage.getItem("__redux__state__");
+	return persistedState ? JSON.parse(persistedState).users : DEFAULT_STATE;
+})();
 
 export const usersSlice = createSlice({
 	name: "users",
 	initialState,
 	reducers: {
-		// Method DELETE
-		deletedUserById: (state, action: PayloadAction<UserId>) => {
+		addNewUser: (state, action: PayloadAction<User>) => {
+			const id = crypto.randomUUID()
+			state.push({ id, ...action.payload })
+		},
+		deleteUserById: (state, action: PayloadAction<UserId>) => {
 			const id = action.payload;
 			return state.filter((user) => user.id !== id);
 		},
+		rollbackUser: (state, action: PayloadAction<UserWithId>) => {
+			const isUserAlreadyDefined = state.some(user => user.id === action.payload.id)
+			if (!isUserAlreadyDefined) {
+				state.push(action.payload)
+			}
+		}
 	},
 });
 
 export default usersSlice.reducer;
-export const { deletedUserById } = usersSlice.actions;
+
+export const { addNewUser, deleteUserById, rollbackUser } = usersSlice.actions;
